d43e68db63a4126be8887ac5091c61034dbff4da
Hilda-Enyioko <enyiokohilda@gmail.com>
2026-09-12 16:51:09 +0100

    chore: task 3: one limit per user

--- programs/solana-fall-transfer-hook/src/instructions/initialize.rs
+++ programs/solana-fall-transfer-hook/src/instructions/initialize.rs
@@ -7,16 +7,16 @@ use crate::{ANCHOR_DISCRIMINATOR_SIZE, RateLimit, error::ErrorCode};
 pub struct Initialize<'info> {
     #[account(mut)]
     pub payer: Signer<'info>,
+    pub mint: InterfaceAccount<'info, Mint>,
     #[account(
         init,
         payer = payer,
         // Unique, program-wide rate limit account. See the CHALLENGE note in
         // `init_extra_account_meta.rs` for making this per-mint/per-owner.
-        seeds = [b"rate_limit"],
+        seeds = [b"rate_limit", mint.key().as_ref(), payer.key().as_ref()],
         bump,
         space = ANCHOR_DISCRIMINATOR_SIZE + RateLimit::INIT_SPACE,
     )]
-    pub mint: InterfaceAccount<'info, Mint>,
     pub rate_limit: Account<'info, RateLimit>,
     pub system_program: Program<'info, System>,
 }
